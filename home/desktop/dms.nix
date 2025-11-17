928c95dc2e0d5bfc7cc2b7e0adc2eaa6dd4a3d70
Archmage-Eternal <davidlobo.dev@gmail.com>
2025-11-17 10:37:57 +0530

    Configured dms

--- home/desktop/dms.nix
+++ home/desktop/dms.nix
@@ -6,6 +6,26 @@
 
   programs.dankMaterialShell = {
     enable = true;
+
+    systemd = { 
+      enable = true;
+      restartIfChanged = true;
+    };
+
+    enableSystemMonitoring = true;
+    enableClipboard = true;            # Clipboard history manager
+    enableVPN = false;                  # VPN management widget
+    enableBrightnessControl = true;    # Backlight/brightness controls
+    enableColorPicker = true;          # Color picker tool
+    enableDynamicTheming = false;       # Wallpaper-based theming (matugen)
+    enableAudioWavelength = true;      # Audio visualizer (cava)
+    enableCalendarEvents = true;       # Calendar integration (khal)
+    enableSystemSound = true;          # System sound effects
+
+    default.settings = {
+      theme = "dark";
+    };
+
     niri = {
       enableKeybinds = true;
       enableSpawn = true;
