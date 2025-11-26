c3cd5ba47c218dacf251e218806dd5daf7e82f9a
Archmage-Eternal <davidlobo.dev@gmail.com>
2025-11-26 20:52:59 +0530

    Added zen browser policies via the home-manager module

--- home/programs/zen-browser.nix
+++ home/programs/zen-browser.nix
@@ -1,11 +1,62 @@
 {
   inputs,
   pkgs,
+  config,
   ...
 }: {
-  imports = [ inputs.zen-browser.homeModules.twilight ];
+  imports = [inputs.zen-browser.homeModules.twilight];
 
   programs.zen-browser = {
     enable = true;
+
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+#
+    # profiles."default" = {
+    #   contianersForce = true;
+    #   containers = {
+    #     Personal = {};
+    #     Work = {};
+    #     Finance = {};
+    #     Shopping = {};
+    #     Study = {};
+    #     Dev = {};
+    #   };
+    #
+    #   spacesForce = true;
+    #   spaces = let
+    #     containers = config.programs.zen-browser.profiles."default".containers;
+    #   in {
+    #     "Home" = {
+    #       id = "";
+    #       icon = "";
+    #       container = containers."".id;
+    #       position = 1234;
+    #     };
+    #
+    #     "Socials" = {};
+    #     "Books" = {};
+    #     "Programming" = {};
+    #     "Nix" = {};
+    #     "Hobbies" = {};
+    #     "Music" = {};
+    #   };
+    # };
   };
 }
