97b8ec329422fbc4f0cdb2ce2b1575c78585d6b5
David Lobo <david.lobo@gehealthcare.com>
2025-11-28 12:37:27 +0530

    Configured yazi

--- home/programs/yazi.nix
+++ home/programs/yazi.nix
@@ -3,5 +3,37 @@
         enable = true;
         enableBashIntegration = true;
         enableNushellIntegration = true;
+
+        # User provided configuration for yazi (home-manager module)
+        settings = {
+            log = {
+                enabled = false;
+            };
+            mgr = {
+                show_hidden = false;
+                sort_by = "natural";
+                sort_dir_first = true;
+                sort_reverse = false;
+                show_symlink = true;
+                linemode = "none";
+                title_format = {cwd};
+            };
+            preview = {
+                wrap = "yes";
+                # max_width = ;
+                # max_height = ;
+                image_filter = "triangle";
+            };
+            # opener = {
+            #     play = [];
+            #     edit = [];
+            #     open = [];
+            # };
+            # open = {
+            #     prepend_rules = [];
+            #     rules = [];
+            #     append_rules = [];
+            # };
+        };
     };
 }
