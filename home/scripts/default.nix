09a9b1a15824f888e95e3eda4a88350515db0476
Archmage-Eternal <davidlobo.dev@gmail.com>
2025-12-05 15:51:34 +0530

    Added protonhax to system scripts

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   home.packages = [
     (pkgs.writeShellScriptBin "daily-note" (builtins.readFile ./daily_note.sh))
+    (pkgs.writeShellScriptBin "protonhax" (builtins.readFile ./protonhax.sh))
   ];
 }
