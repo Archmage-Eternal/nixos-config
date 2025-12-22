36a6dcca39cde68cb8d3c170c983f9cf87a08031
Archmage-Eternal <davidlobo.dev@gmail.com>
2025-12-22 14:54:24 +0530

    Fixes after new installation

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,12 +1,12 @@
 {...}: {
   imports = [
-  ./dms.nix
-  ./dsearch.nix
-  ./niri
-  ./playerctl.nix
-  ./poweralertd.nix
-  ./stylix.nix
-  ./utils.nix
-  ./xdg-mimes.nix
+    ./dms.nix
+    ./dsearch.nix
+    ./niri
+    ./playerctl.nix
+    ./poweralertd.nix
+    ./stylix.nix
+    ./utils.nix
+    ./xdg-mimes.nix
   ];
 }
