03301a0e7d7947ba61e32335f9d4d04a4966c871
Archmage-Eternal <davidlobo.dev@gmail.com>
2026-02-10 11:55:33 +0530

    Updated git config to fix eval warnings. Seperated delta config into its own file

---
+++ home/dev/delta.nix
@@ -0,0 +1,13 @@
+  {...}: {
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      line-numbers = true;
+      side-by-side = false;
+      diff-so-fancy = true;
+      navigate = true;
+    };
+  };
+}
--- home/dev/git.nix
+++ home/dev/git.nix
@@ -1,17 +1,17 @@
-{ pkgs, username, ... }:
+{ pkgs, ... }:
 {
   programs.git = {
     enable = true;
-
-    userName = "Archmage-Eternal";
-    userEmail = "davidlobo.dev@gmail.com";
-
-    extraConfig = {
-      init.defaultBranch = "main";
-      merge.conflictstyle = "diff3";
-      diff.colorMoved = "default";
-      pull.ff = "only";
-      color.ui = true;
+    settings = {
+      user = {
+        name = "Archmage-Eternal";
+        email = "davidlobo.dev@gmail.com";
+      };
+      init = { defaultBranch = "main"; };
+      merge = { conflictstyle = "diff3"; };
+      diff = { colorMoved = "default"; };
+      pull = { ff = "only"; };
+      color = { ui = true; };
       url = {
         "git@github.com:".insteadOf = [
           "gh:"
@@ -19,16 +19,7 @@
         ];
       };
     };
-
-    delta = {
-      enable = true;
-      options = {
-        line-numbers = true;
-        side-by-side = false;
-        diff-so-fancy = true;
-        navigate = true;
-      };
-    };
   };
+
   home.packages = [ pkgs.gh ]; # pkgs.git-lfs
 }
