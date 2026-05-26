af9eef8451dd3749f2bd4aaa66ab0165f5799623
Archmage-Eternal <davidlobo.dev@gmail.com>
2026-05-26 10:00:15 +0530

    Split bash config form shell.nix

---
+++ modules/shell/bash.nix
@@ -0,0 +1,20 @@
+{...}: {
+  flake = {
+    nixosModules.shell.programs.bash = {
+      enable = true;
+      completion.enable = true;
+      blesh.enable = true;
+    };
+    homeModules.shell.programs.bash = {
+      enable = true;
+      enableCompletion = true;
+      initExtra = ''
+        set -o vi
+      '';
+      shellAliases = {
+        ll = "ls -lah";
+        ".." = "cd ..";
+      };
+    };
+  }
+}
